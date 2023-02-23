d41f57521bcda319713d2dbe0e77dab9a7997ed0
Bahrom10 <125563633+Bahrom10@users.noreply.github.com>
2023-02-23 16:57:50 +0500

    Update script3.js

--- script3.js
+++ script3.js
@@ -2,6 +2,6 @@ str1 = +prompt('Введите 1 число')
 str2 = +prompt('Введите 2 число')
 str3 = +prompt('Введите 3 число')
 
-if(str1 < str2 < str3 || str3 < str2 < str1) alert(str2)
+if(str1 < str2 < str3 || str3 < str2 < str1) alert(str1)
 else if(str1 < str3 < str2 || str2 < str3 < str1) alert(str3)
 else if(str3 < str1 < str2 || str2 < str1 < str3) alert(str2)
